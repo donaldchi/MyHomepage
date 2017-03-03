06899a7039136bf445f6fa428877db274887f559
chi <chiguanglong0@gmail.com>
2017-03-03 14:41:10 +0900

    one page navigationの追加

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -17,8 +17,63 @@ $(window).scroll(function(){
 // ◇ボタンをクリックしたら、スクロールして上に戻る
 topBtn.click(function(){
   $('body,html').animate({
-  scrollTop: 0},500);
+  scrollTop: 0},800);
   return false;
 });
 
+});
+
+$(function(){
+    var aboutBtn=$('#aboutmeBtn');
+    var projectBtn=$('#projectBtn');
+    var educationBtn=$('#educationBtn');
+    var blogBtn=$('#blogBtn');
+    var githubBtn=$('#githubBtn');
+    var musicBtn=$('#musicBtn');
+    var serviceBtn=$('#serviceBtn');
+
+    aboutBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#aboutme").offset().top
+        }, 800);
+        return false;
+    });
+
+    projectBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#project").offset().top
+        }, 800);
+        return false;
+    });
+
+    educationBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#education").offset().top
+        }, 800);
+        return false;
+    });
+    blogBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#blog").offset().top
+        }, 800);
+        return false;
+    });
+    githubBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#github").offset().top
+        }, 800);
+        return false;
+    });
+    serviceBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#service").offset().top
+        }, 800);
+        return false;
+    });
+    musicBtn.click(function(){
+        $('html, body').animate({
+          scrollTop: $("#music").offset().top
+        }, 800);
+        return false;
+    });
 });
